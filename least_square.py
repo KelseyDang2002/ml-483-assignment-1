a87b520015dbbf69d8b81f3cad207b8247c6299a
KelseyDang2002 <kelseydang813@gmail.com>
2024-09-20 22:33:11 -0700

    TODO: preprocessing

--- least_square.py
+++ least_square.py
@@ -2,72 +2,74 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics import r2_score, mean_squared_error as mse
 
+TEST_DATA_PERCENTAGE = 0.3 # 30% testing and 70% training
+POLYNOMIAL_ORDER = 1
+
+'''Main'''
 def main():
+    X_train, X_test, y_train, y_test = split_data()
+    linear_regression_model(X_train, X_test, y_train, y_test)
+
+'''Split into testing and trainnig data'''
+def split_data():
     # read csv file into dataframe
     df = pd.read_csv('wine_data.csv', sep=';')
-
-    # print dataframe
     print(f"Dataframe:\n{df}\n")
 
-    # check for null values
-    # print(f"Check for null values:\n{df.isnull().sum()}\n")
-
-    # drop lablel column to only get the features
-    features = df.drop(columns='quality')
-    print(f"Training Features:\n{features}\n")
+    # drop label column to only get the features
+    x = df.drop(['quality'], axis=1).values
+    print(f"Training Features:\n{x}\n")
 
     # label column
-    labels = df['quality']
-    print(f"Labels:\n{labels}\n")
+    y = df['quality'].values
+    print(f"Labels (quality):\n{y}\n")
 
-    # split data into 50% testing and 50% training
-    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.5, random_state=0)
+    # split data
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=TEST_DATA_PERCENTAGE, random_state=0)
+    return X_train, X_test, y_train, y_test
 
-    # preprocess data
-    scalar = preprocessing.StandardScaler().fit(X_train)
+'''Plot'''
+def plot(x_axis, y_axis, title):
+    plt.scatter(x_axis, y_axis)
+    plt.xlabel("Actual Quality")
+    plt.ylabel("Predicted Quality")
+    plt.title(title)
+    plt.show()
 
+'''Linear Regression'''
+def linear_regression_model(X_train, X_test, y_train, y_test):
     # linear regression model
     lr = LinearRegression()
+
+    # preprocess training data TODO
+    poly = PolynomialFeatures(degree=POLYNOMIAL_ORDER, interaction_only=False)
+    poly.fit_transform(X_train)
 
     # train the model with X_train and y_train
     lr.fit(X_train, y_train)
     
     # print y-intercept (b in mx + b)
     print(f"y-intercept: {lr.intercept_}\n")
-    
     # print coefficients (m in mx + b), there should be 11 coefficients for the 11 training features
     print(f"11 Coefficients:\n{lr.coef_}\n")
-    
-    # training is done, now we test by predicting quality based on X_train
-    y_pred_train = lr.predict(X_train)
-    print(f"Predicted Training Values:\n{y_pred_train}\n")
-
-    # now we plot to visualize how good the predictions are
-    # compare actual quality (y_train) vs predicted (y_pred_train)
-    # plt.scatter(y_train, y_pred_train)
-    # plt.xlabel("Actual Quality")
-    # plt.ylabel("Predicted Quality")
-    # plt.show()
-
-    # measure prediction with R Squared
-    print(f"Training R-Squared Score: {r2_score(y_train, y_pred_train)}\n")
 
     # predict quality based on X_test
     y_pred_test = lr.predict(X_test)
-    print(f"Predicted Testing Values:\n{y_pred_test}\n")
+    plot(y_test, y_pred_test, "Testing")
+    print(f"Testing R-Squared Score (close to 1 = better):\t{r2_score(y_test, y_pred_test)}")
+    print(f"Testing RMSE Score (close to 0 = better):\t{np.sqrt(mse(y_test, y_pred_test))}\n")
+    
+    # predict quality based on X_train
+    y_pred_train = lr.predict(X_train)
+    plot(y_train, y_pred_train, "Training")
+    print(f"Training R-Squared Score (close to 1 = better):\t{r2_score(y_train, y_pred_train)}")
+    print(f"Training RMSE Score (close to 0 = better):\t{np.sqrt(mse(y_train, y_pred_train))}\n")
 
-    # plt.scatter(y_test, y_pred_test)
-    # plt.xlabel("Actual Quality")
-    # plt.ylabel("Predicted Quality")
-    # plt.show()
-
-    # measure prediction with R Squared
-    print(f"Testing R-Squared Score: {r2_score(y_test, y_pred_test)}\n")
-
+'''Call main'''
 if __name__ == "__main__":
     main()
