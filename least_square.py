7d43e2e3d2951c7dc60230b63fa7922b455a36d6
KelseyDang2002 <kelseydang813@gmail.com>
2024-09-23 21:32:38 -0700

    TODO: test split ratios & answer questions

--- least_square.py
+++ least_square.py
@@ -4,16 +4,38 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score, mean_squared_error as mse
+from sklearn.metrics import r2_score, mean_squared_error
 import time
 
 TEST_DATA_PERCENTAGE = 0.3 # 30% testing and 70% training
-POLYNOMIAL_ORDER = 5 # change order for testing
+POLYNOMIAL_ORDER = 8
 
 '''Main'''
 def main():
-    X_train, X_test, y_train, y_test = split_data()
-    linear_regression_model(X_train, X_test, y_train, y_test)
+    x, y = features_and_label()
+    X_train, X_test, y_train, y_test = split_data(x, y)
+    # Least Square with sklearn
+    order = 1
+    while order <= POLYNOMIAL_ORDER:
+        try:
+            least_square_sklearn(X_train, X_test, y_train, y_test, order)
+            order += 1
+        
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt\n")
+            break
+
+    # Least Square manual
+    # X_train_scaled, X_test_scaled, X_train_poly, X_test_poly = preprocess_dataset(X_train, X_test)
+    # X_train_poly = np.hstack([np.ones((X_train_scaled.shape[0], 1)), X_train_scaled])  # Add intercept term
+    # w_manual = least_squares_manual(X_train_poly, y_train)
+    # print(f"Manual Least Squares Weight ({len(w_manual)}): {w_manual}\n")
+    # 
+    # y_pred_train_manual = w_manual.predict(X_train_poly)
+    # train_rmse = np.sqrt(mean_squared_error(y_train, y_pred_train_manual))
+    # print(f"Training RMSE Score (close to 0 = better):\t{train_rmse:.6f}")
+    # train_r2 = r2_score(y_train, y_pred_train_manual)
+    # print(f"Training R-Squared Score (close to 1 = better):\t{train_r2:.6f}\n")
 
 '''Plot'''
 def plot(x_axis, y_axis, title, x_label, y_label):
@@ -24,18 +46,12 @@ def plot(x_axis, y_axis, title, x_label, y_label):
     plt.ylabel(y_label)
     plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     plt.yticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    # plt.yticks([0, 1])
     plt.show()
 
-# TODO
+'''Get training features and label column from data set'''
 def features_and_label():
-    return
-
-'''Split into testing and trainnig data'''
-def split_data():
     # read csv file into dataframe
     df = pd.read_csv('wine_data.csv', sep=';')
-    # print(f"Dataframe:\n{df}\n")
         
     # check for any null or missing values
     print(f"Check for null or missing values:\n{df.isnull().sum()}\n")
@@ -58,26 +74,51 @@ def split_data():
 
     # label only column
     y = df['quality'].values
-    print(f"Labels (quality):\n{y}\n")
+    print(f"Labels (quality):\n{y}\n") 
+    return x, y
 
-    # split data
+'''Split into testing and trainnig data'''
+def split_data(x, y):
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=TEST_DATA_PERCENTAGE, random_state=0)
     return X_train, X_test, y_train, y_test
 
+# '''Preprocess dataset'''
+# def preprocess_dataset(X_train, X_test):
+#     # preprocess data
+#     scalar = StandardScaler()
+#     X_train_scaled = scalar.fit_transform(X_train)
+#     X_test_scaled = scalar.fit_transform(X_test)
+
+#     # polynomial features
+#     # order = 1
+#     poly = PolynomialFeatures(degree=POLYNOMIAL_ORDER, interaction_only=False, include_bias=False)
+#     X_train_poly = poly.fit_transform(X_train_scaled)
+#     X_test_poly = poly.fit_transform(X_test_scaled)
+
+#     print("-" * 75)
+#     print(f"Polynomial Order: {POLYNOMIAL_ORDER}\n")
+
+#     return X_train_scaled, X_test_scaled, X_train_poly, X_test_poly
+
 '''Linear Regression'''
-def linear_regression_model(X_train, X_test, y_train, y_test):
+def least_square_sklearn(X_train, X_test, y_train, y_test, order):
     # linear regression model
     lr = LinearRegression()
 
+    # X_train_scaled, X_test_scaled, X_train_poly, X_test_poly = preprocess_dataset(X_train, X_test)
     # preprocess data
     scalar = StandardScaler()
     X_train_scaled = scalar.fit_transform(X_train)
     X_test_scaled = scalar.fit_transform(X_test)
 
     # polynomial features
-    poly = PolynomialFeatures(degree=POLYNOMIAL_ORDER, interaction_only=False, include_bias=False)
+    # order = 1
+    poly = PolynomialFeatures(degree=order, interaction_only=False, include_bias=False)
     X_train_poly = poly.fit_transform(X_train_scaled)
     X_test_poly = poly.fit_transform(X_test_scaled)
+
+    print("-" * 75)
+    print(f"Polynomial Order: {order}\n")
 
     start = time.time()
 
@@ -85,12 +126,9 @@ def linear_regression_model(X_train, X_test, y_train, y_test):
     lr.fit(X_train_poly, y_train)
     
     end = time.time()
-    print("-" * 75)
     print(f"Training time: {end - start} seconds\n")
 
     print(f"Testing Data: {TEST_DATA_PERCENTAGE * 100}%\nTraining Data: {100 - (TEST_DATA_PERCENTAGE * 100)}%\n")
-
-    print(f"Polynomial Order: {POLYNOMIAL_ORDER}\n")
     
     # print y-intercept (b in mx + b)
     print(f"y-intercept: {lr.intercept_}")
@@ -100,24 +138,25 @@ def linear_regression_model(X_train, X_test, y_train, y_test):
     # predict quality based on X_train
     y_pred_train = lr.predict(X_train_poly)
     # plot(y_train, y_pred_train, "Training", "Actual", "Predicted")
-    print(f"Training RMSE Score (close to 0 = better):\t{np.sqrt(mse(y_train, y_pred_train)):.6f}")
-    print(f"Training R-Squared Score (close to 1 = better):\t{r2_score(y_train, y_pred_train):.6f}\n")
+    train_rmse = np.sqrt(mean_squared_error(y_train, y_pred_train))
+    print(f"Training RMSE Score (close to 0 = better):\t{train_rmse:.6f}")
+    train_r2 = r2_score(y_train, y_pred_train)
+    print(f"Training R-Squared Score (close to 1 = better):\t{train_r2:.6f}\n")
     
     # predict quality based on X_test
     y_pred_test = lr.predict(X_test_poly)
     # plot(y_test, y_pred_test, "Testing", "Actual", "Predicted")
-    print(f"Testing RMSE Score (close to 0 = better):\t{np.sqrt(mse(y_test, y_pred_test)):.6f}")
-    print(f"Testing R-Squared Score (close to 1 = better):\t{r2_score(y_test, y_pred_test):.6f}\n")
+    test_rmse = np.sqrt(mean_squared_error(y_test, y_pred_test))
+    print(f"Testing RMSE Score (close to 0 = better):\t{test_rmse:.6f}")
+    test_r2 = r2_score(y_test, y_pred_test)
+    print(f"Testing R-Squared Score (close to 1 = better):\t{test_r2:.6f}\n")
+    print("-" * 75)
 
-# TODO
-# def least_squares_solution(x, y):
+# '''Least Square Method without using sklearn'''
+# def least_squares_manual(x, y):
 #     X_T_X_inv = np.linalg.inv(x.T.dot(x))
 #     w = X_T_X_inv.dot(x.T).dot(y)
 #     return w
-
-# Example: Using training data (for linear model) TODO
-# X_train_poly = np.hstack([np.ones((X_train_scaled.shape[0], 1)), X_train_scaled])  # Add intercept term
-# w_manual = least_squares_solution(X_train_poly, y_train)
 
 '''Call main'''
 if __name__ == "__main__":
